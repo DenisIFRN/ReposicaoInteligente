e32dae42ed3036bc98494e52d6a397902e77f4fb
DENIS SOARES <SOARESDENIS08@GMAIL.COM>
2019-12-10 08:32:45 -0300

    comentando auth

--- app/Http/Controllers/ProfessorController.php
+++ app/Http/Controllers/ProfessorController.php
@@ -18,9 +18,9 @@ class ProfessorController extends Controller
     {
         if(Session::has('user')) {
             $sessao = Session::get('user');
-            if($sessao['2'] != 'Docente'){
+            /* if($sessao['2'] != 'Docente'){
                 return redirect()->to(route('loginForm'));
-            }
+            }*/
         }else{
             return redirect()->to(route('loginForm'));
         }
